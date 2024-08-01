5a1293f22ce8d31ff30ede576595055f70fa7e37
mufkhalif <mufkalif18@gmail.com>
2024-08-01 11:55:20 +0700

    Initial Commit

---
+++ Pokedex/PokedexApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokedexApp.swift
+//  Pokedex
+//
+//  Created by mufkhalif on 01/08/24.
+//
+
+import SwiftUI
+
+@main
+struct PokedexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
